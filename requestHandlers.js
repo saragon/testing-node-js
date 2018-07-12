6e6d6b99d28fcc237c37f3a1ba1eaa89b24e0668
Unknown <sara.aragonp@gmail.com>
2018-07-12 11:35:03 +0200

    A very simple HTTP server with nodeJs

---
+++ requestHandlers.js
@@ -0,0 +1,62 @@
+function iniciar(response) {
+  console.log("Manipulador de peticiones 'iniciar' fue llamado.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/subir" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Enviar texto" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function subir(response) {
+  console.log("Manipulador de peticiones 'subir' fue llamado.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hola Subir");
+  response.end();
+}
+
+exports.iniciar = iniciar;
+exports.subir = subir;
+
+var body2 = function iniciar(response, postData) {
+        console.log("Manipulador de Peticion 'iniciar' fue llamado.");
+
+        var body = '<html>'+
+          '<head>'+
+          '<meta http-equiv="Content-Type" content="text/html + charset=UTF-8" />'+
+          '</head>'+
+          '<body>'+
+          '<form action="/subir" method="post">'+
+          '<textarea name="text" rows="20" cols="60"></textarea>'+
+          '<input type="submit" value="Enviar texto" />'+
+          '</form>'+
+          '</body>'+
+          '</html>';
+        
+          response.writeHead(200, {"Content-Type": "text/html"});
+          response.write(body);
+          response.end();
+      }
+
+      function subir(response, dataPosteada) {
+        console.log("Manipulador de Peticion 'subir' fue llamado.");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("Tu enviaste: " + dataPosteada);
+        response.end();
+      }
+
+    exports.iniciar = iniciar;
+    exports.subir = subir;
+    }
+  }
+}
